8b5444e0634c347b59065f7f566f4451174cbd38
Yogadimas <yogadimaspambudi@gmail.com>
2023-10-28 03:58:55 +0700

    Struktur Folder, Routes & View

---
+++ public/js/script.js
@@ -0,0 +1,1 @@
+alert("Hello Yoga Dimas");
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Yoga Dimas",
+        "email" => "yogadimaspambudi@gmail.com",
+        "image" => "yd.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
